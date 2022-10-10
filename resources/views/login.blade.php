9e8005009524d4f30fbbc717192b5d3ad6e6d016
iamrahul1973 <rahulkr@gmail.com>
2022-10-10 21:39:51 +0530

    ADD HTML VALIDATION TO LOGIN FORM

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,14 +2,14 @@
     <form method="POST" action="/login">
         <div>
             <label for="email">Username : </label>
-            <input type="text" name="email" id="email" />
+            <input type="text" name="email" id="email" required />
         </div>
         @error('email')
             <p style="color: red; font-size: 14px">{{ $message }}</p>
         @enderror
         <div>
             <label for="password">Password : </label>
-            <input type="text" name="password" id="password" />
+            <input type="text" name="password" id="password" required />
         </div>
         @error('password')
             <p style="color: red; font-size: 14px">{{ $message }}</p>
